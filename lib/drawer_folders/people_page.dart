dd72b5b3928cfe02bcd4c2d905dfee27bf0b1aec
IstiaqOsman <istiaq.workmail@gmail.com>
2021-11-04 00:52:45 +0600

    Sheikh Hasina Projects update

--- lib/drawer_folders/people_page.dart
+++ lib/drawer_folders/people_page.dart
@@ -3,8 +3,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget people_page(){
-  String img = "assets/beauty.jpeg";
-  return Scaffold(appBar: AppBar(
+  String img = "assets/sheikh_hasina/sheikh_hasina.jpg";
+  String img2 = "assets/sheikh_hasina/rohingya_shelter.jpg";
+  String img3 = "assets/sheikh_hasina/padma_bridge.jpg";
+  String img4 = "assets/sheikh_hasina/sh_university.jpg";
+
+  return Scaffold(
+      appBar: AppBar(
     // leading: IconButton(
     //   onPressed: (){
     //
@@ -12,192 +17,86 @@ Widget people_page(){
     //   icon: const Icon(Icons.menu),
     //   // tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip, icon: const Icon(Icons.menu),onPressed: (){},
     // ),
-    title: Text('AppBar'),
-    actions: [
-      IconButton(
-          tooltip: 'Favourite', icon: const Icon(Icons.favorite,),onPressed: (){}
-      ),
-      IconButton(
-          tooltip: 'Search', icon: const Icon(Icons.search,),onPressed: (){}
-      ),
-      PopupMenuButton<Text>(
-          itemBuilder: (context){
-            return [
-              PopupMenuItem(
-                child: Text('First One'),
-              ),
-              PopupMenuItem(
-                child: Text('Second One'),
-              ),
-              PopupMenuItem(
-                child: Text('Third One'),
-              ),
-            ];
-          })
-    ],
+        title: Text('AppBar'),
+        actions: [
+          IconButton(
+              tooltip: 'Favourite', icon: const Icon(Icons.favorite,),onPressed: (){}
+          ),
+          IconButton(
+              tooltip: 'Search', icon: const Icon(Icons.search,),onPressed: (){}
+          ),
+          PopupMenuButton<Text>(
+              itemBuilder: (context){
+                return [
+                  PopupMenuItem(
+                    child: Text('First One'),
+                  ),
+                  PopupMenuItem(
+                    child: Text('Second One'),
+                  ),
+                  PopupMenuItem(
+                    child: Text('Third One'),
+                  ),
+                ];
+              })
+        ],
     // title: Text('View'),
     // elevation: 10,
     // shadowColor: Colors.green,
-  ),body: SingleChildScrollView(
-    child: Container(
+  ),
+      body: SingleChildScrollView(
+      child: Container(
+        height: 1200,
       width: double.infinity,
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.only(top: 60),
       color: Colors.white,
       child: Column(
         children: <Widget>[
-          Center(
-            child: Container(
-                height: 375,
-                width: 250,
-                child: Image.asset(img)
-            ),
+          Expanded(
+            flex: 3,
+          child: Container(
+              height: 310,
+              width: double.infinity,
+              child: Center(
+                  child: Image.asset(img, fit: BoxFit.cover))),
           ),
           Container(
-              margin: EdgeInsets.all(2),
-              height: 200,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                      flex: 1,
-                      child: FlatButton(
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          height: 160,
-                          child: Card(
-                            shadowColor: Colors.red,
-                            elevation: 10,
-                            shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: Colors.green, width: 1)),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.home,
-                                    size: 50,
-                                  ),
-                                  Divider(
-                                    color: Colors.red,
-                                  ),
-                                  Text('Home',style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        onPressed: (){},)
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: FlatButton(
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          height: 160,
-                          child: Card(
-                            shadowColor: Colors.red,
-                            elevation: 10,
-                            shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: Colors.green, width: 1)),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.tour,
-                                    size: 50,
-                                  ),
-                                  Divider(
-                                    color: Colors.red,
-                                  ),
-                                  Text('Tour',style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        onPressed: (){},
-                      )
-                  ),
-                ],
-              )
-          ),
-          Center(
-            child: Container(
-                height: 375,
-                width: 250,
-                child: Image.asset(img)
-            ),
-          ),
-          Container(
-              margin: EdgeInsets.all(2),
-              height: 200,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: FlatButton(
-                      child: Container(
-                        padding: EdgeInsets.all(3),
-                        height: 160,
-                        child: Card(
-                          shadowColor: Colors.red,
-                          elevation: 10,
-                          shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: Colors.green, width: 1)),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.hotel,
-                                  size: 50,
-                                ),
-                                Divider(
-                                  color: Colors.red,
-                                ),
-                                Text('Hotels',style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      onPressed: (){},
+            child: Column(
+              children: [
+                Container(
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      children: [
+                        Image.asset(img2),
+                        Text("Padma Bridge Project", style: TextStyle(fontWeight: FontWeight.bold),)
+                      ],
                     ),
                   ),
-                  Expanded(
-                      flex: 1,
-                      child: FlatButton(
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          height: 160,
-                          child: Card(
-                            shadowColor: Colors.red,
-                            elevation: 10,
-                            shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide: BorderSide(color: Colors.green, width: 1)),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.contacts,
-                                    size: 50,
-                                  ),
-                                  Divider(
-                                    color: Colors.red,
-                                  ),
-                                  Text('Contact Us',style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        onPressed: (){},
-                      )
+                ),
+                Container(
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      children: [
+                        Image.asset(img3),
+                        Text("Rohingya Shelter Project", style: TextStyle(fontWeight: FontWeight.bold),)
+                      ],
+                    ),
                   ),
-                ],
-              )
+                ),
+                Container(
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      children: [
+                        Image.asset(img4),
+                        Text("Sheikh Hasina University", style: TextStyle(fontWeight: FontWeight.bold),)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
